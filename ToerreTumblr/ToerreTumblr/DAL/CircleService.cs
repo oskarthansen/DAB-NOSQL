6844ec56856e9572faf361e51b8a8a57cfd25c8f
Valdemartt <36989028+Valdemartt@users.noreply.github.com>
2019-05-13 11:44:34 +0200

    Update

---
+++ ToerreTumblr/ToerreTumblr/DAL/CircleService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using ToerreTumblr.Models;
+
+namespace ToerreTumblr.DAL
+{
+    public class CircleService
+    {
+        private readonly IMongoCollection<Circle> _books;
+
+        public CircleService(IConfiguration config)
+        {
+            var client = new MongoClient(config.GetConnectionString("BookstoreDb"));
+            var database = client.GetDatabase("BookstoreDb");
+            _books = database.GetCollection<Circle>("Books");
+        }
+    }
+}
