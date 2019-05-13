bc65ad1e3ca84e5d0afd685add90db7ed0c380f9
Thomas <thomas.moeller.1996@gmail.com>
2019-05-13 11:45:12 +0200

    Added identity customization

---
+++ ToerreTumblr/ToerreTumblr/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ToerreTumblr.Data;
+
+[assembly: HostingStartup(typeof(ToerreTumblr.Areas.Identity.IdentityHostingStartup))]
+namespace ToerreTumblr.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
