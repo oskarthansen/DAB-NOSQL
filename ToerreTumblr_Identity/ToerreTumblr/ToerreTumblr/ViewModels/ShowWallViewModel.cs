2d91856443a1b06c2f302fa9c26b57982b7e4de5
Thomas <thomas.moeller.1996@gmail.com>
2019-05-13 15:09:35 +0200

    Create ShowWallViewModel.cs

---
+++ ToerreTumblr_Identity/ToerreTumblr/ToerreTumblr/ViewModels/ShowWallViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToerreTumblr.Models;
+
+namespace ToerreTumblr.ViewModels
+{
+    public class ShowWallViewModel
+    {
+        public List<Post> Posts { get; set; }
+        public User User { get; set; }
+    }
+}
