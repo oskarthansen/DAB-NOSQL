56c185846f94f8cea032a3f86a61b81872864cc9
Valdemartt <36989028+Valdemartt@users.noreply.github.com>
2019-05-13 15:08:52 +0200

    Added view for Feed

--- ToerreTumblr_Identity/ToerreTumblr/ToerreTumblr/Controllers/UserController.cs
+++ ToerreTumblr_Identity/ToerreTumblr/ToerreTumblr/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using ToerreTumblr.DAL;
@@ -11,15 +12,15 @@ namespace ToerreTumblr.Controllers
 {
     public class UserController : Controller
     {
-        private UserService _repo
+        private UserService _repo;
         public UserController(IConfiguration config)
         {
             _repo = new UserService(config);
         }
         public async Task<IActionResult> ShowFeed()
         {
-            List<Post> feed = _repo.GetFeed()
-            return View();
+            List<Post> feed = _repo.GetFeed(HttpContext.Session.GetString("UserId"));
+            return View(feed);
         }
 
         public IActionResult AddPost()
@@ -36,8 +37,8 @@ namespace ToerreTumblr.Controllers
 
         public async Task<IActionResult> ShowWall(string id)
         {
-            List<Post> wallPosts = new List<Post>();
-            return View();
+            List<Post> wallPosts = _repo.GetWall(id);
+            return View(wallPosts);
         }
     }
 }
