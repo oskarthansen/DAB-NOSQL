8ce7530955fe3f2dda735a900c7a32e5fb9c5aeb
Thomas <thomas.moeller.1996@gmail.com>
2019-05-13 11:53:50 +0200

    Added identity customize

--- ToerreTumblr/ToerreTumblr/Data/ApplicationDbContext.cs
+++ ToerreTumblr/ToerreTumblr/Data/ApplicationDbContext.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ToerreTumblr.Models;
 
 namespace ToerreTumblr.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<User>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
